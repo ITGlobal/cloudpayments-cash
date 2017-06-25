20be6f01687ec6d37ceba23eb484e0769e9b4a29
Rodion Shergin <beholderrk@gmail.com>
2017-06-25 19:30:50 +0300

    appveyor build script

---
+++ build.ps1
@@ -0,0 +1,22 @@
+$CONFIGURATION = $env:CONFIGURATION
+$BUILD_FOLDER = $env:APPVEYOR_BUILD_FOLDER
+$PROJECT = Join-Path $BUILD_FOLDER 'CloudPayments.Cash'
+$OUTDIR = Join-Path $BUILD_FOLDER 'artifacts'
+
+Write-Host "Restoring packages" -ForegroundColor Cyan
+& dotnet restore $BUILD_FOLDER
+if($LASTEXITCODE) {
+    exit 1
+}
+
+Write-Host "Building project" -ForegroundColor Cyan
+& dotnet build $BUILD_FOLDER -c $CONFIGURATION
+if($LASTEXITCODE) {
+    exit 1
+}
+
+Write-Host "Packaging project" -ForegroundColor Cyan
+& dotnet pack $PROJECT -c $CONFIGURATION -o $OUTDIR
+if($LASTEXITCODE) {
+    exit 1
+}
