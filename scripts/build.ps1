ce83284629cd5a7732ad1a9eeefbf2476f50e815
Rodion Shergin <beholderrk@gmail.com>
2017-06-26 14:02:18 +0300

    fix build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,6 +2,9 @@ $CONFIGURATION = $env:CONFIGURATION
 $BUILD_VERSION = if($env:APPVEYOR_REPO_TAG) { $env:APPVEYOR_REPO_TAG_NAME } else { $env:APPVEYOR_BUILD_VERSION }
 $BUILD_FOLDER = $env:APPVEYOR_BUILD_FOLDER
 
+Write-Host "version: $BUILD_VERSION" -ForegroundColor Yellow
+Write-Host "folder: $BUILD_FOLDER" -ForegroundColor Yellow
+
 Write-Host "Restoring packages" -ForegroundColor Cyan
 & dotnet restore $BUILD_FOLDER /p:Version=$BUILD_VERSION
 if($LASTEXITCODE) {
