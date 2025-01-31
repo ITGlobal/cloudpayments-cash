bc5e543333aceba4f28303fcaf69284293e67e8a
qoolek <qoolek@gmail.com>
2025-01-31 11:48:23 +0300

    Update VatValues.cs
    Add new VAT values

--- CloudPayments.Cash/Contracts/VatValues.cs
+++ CloudPayments.Cash/Contracts/VatValues.cs
@@ -1,14 +1,21 @@
 ﻿namespace CloudPayments.Cash.Contracts
 {
     /// <summary>
-    /// see https://cloudpayments.ru/docs/api/kassa
+    /// see https://developers.cloudkassir.ru/#vat (old link https://cloudpayments.ru/docs/api/kassa)
     /// </summary>
     public enum VatValues
     {
         P0 = 0,
+        P5 = 5,
+        P7 = 7,
         P10 = 10,
+        P12 = 12,
         P18 = 18,
+        P20 = 20,
+        P105 = 105,
+        P107 = 107,
         P110 = 110,
         P118 = 118,
+        P120 = 120,
     }
 }
